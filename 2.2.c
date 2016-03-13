3410a0b9a298aa18b54ab53a59b65c96d496329b
Edward Hernandez <ehernandez@email.wm.edu>
2016-03-12 22:18:29 -0500

    added 2.2

---
+++ 2.2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/*
+ * replace the following loop without AND or OR
+ *	for(i = 0; i < lim-1 && (c=getchar()) != '\n' && c != EOF; ++i)
+ *		s[i] = c;
+ */
+
+#define MAXLINE 1000
+
+int main(void)
+{
+	int i, c, lim;
+	/* char s[MAXLINE]; */
+	i = 0;
+	lim = MAXLINE;
+
+	for(i = 0; i < lim-1; ++i)
+	{
+		c = getchar();
+
+		if(c == '\0')
+		{
+			printf("\n");
+			return 0;
+		}
+		if(c == '\n')
+		{
+			printf("\n");
+			return 0;
+		}
+
+		putchar(c);
+	}
+	return 0;
+}
